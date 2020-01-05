a69e7c5f8e0ef5fd27fe290874d3292c16d532e8
Grzegorz Tymiński <grzegorz.tyminski@MacBook-Pro-GT.local>
2020-01-05 16:37:37 +0100

    Extended Kalman Filter implementation.

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,5 +1,6 @@
 #include "tools.h"
 #include <iostream>
+#include <math.h>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
@@ -11,14 +12,76 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-   * TODO: Calculate the RMSE here.
-   */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+    if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+        // cout << "Invalid estimation or ground_truth data" << endl;
+        return rmse;
+    }
+
+    // accumulate squared residuals
+    for (unsigned int i = 0; i < estimations.size(); ++i) {
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+
+    // calculate the mean
+    rmse = rmse / estimations.size();
+    // calculate the squared root
+    rmse = rmse.array().sqrt();
+    // return the result
+    return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
+    MatrixXd Hj(3, 4);
+    // recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    float px_py_2 = px * px + py * py;
+    float sqrt_px_py_2 = sqrt(px_py_2);
+    float vxpy_vypx = vx * py - vy * px;
+    float vypx_vxpy = -vxpy_vypx;
+    float px_py_2_32 = pow(px_py_2, 3) / sqrt_px_py_2;
+
+    // check division by zero
+    if (fabs(px_py_2) < 0.00001) {
+        return Hj;
+    }
+
+    // compute the Jacobian matrix
+    Hj <<   (px / sqrt_px_py_2),            (py / sqrt_px_py_2),            0, 0,
+            (-py / px_py_2),                (px / px_py_2),                 0, 0,
+            (py * vxpy_vypx / px_py_2_32),  (px * vypx_vxpy / px_py_2_32),  (px / sqrt_px_py_2), (py / sqrt_px_py_2);
+
+    return Hj;
+}
+
+VectorXd Tools::Polar2Cartesian(const MeasurementPackage &measurement_pack) {
+    VectorXd result = VectorXd(4);
+    double rho = measurement_pack.raw_measurements_[0]; // range
+    double phi = measurement_pack.raw_measurements_[1]; // bearing
+    double rho_dot = measurement_pack.raw_measurements_[2]; // velocity
+
+    double x = Tools::verifyZero(rho * cos(phi));
+    double y = Tools::verifyZero(rho * sin(phi));
+
+    double vx = rho_dot * cos(phi);
+    double vy = rho_dot * sin(phi);
+
+    result << x, y, vx, vy;
+    return result;
+}
+
+double Tools::verifyZero(double value) {
+    double treshold = 0.001;
+    if (value < treshold){
+        return treshold;
+    }
+    return value;
 }
--- src/tools.h
+++ src/tools.h
@@ -3,30 +3,44 @@
 
 #include <vector>
 #include "Eigen/Dense"
+#include "measurement_package.h"
 
 class Tools {
- public:
-  /**
-   * Constructor.
-   */
-  Tools();
+public:
+    /**
+     * Constructor.
+     */
+    Tools();
 
-  /**
-   * Destructor.
-   */
-  virtual ~Tools();
+    /**
+     * Destructor.
+     */
+    virtual ~Tools();
 
-  /**
-   * A helper method to calculate RMSE.
-   */
-  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
-                                const std::vector<Eigen::VectorXd> &ground_truth);
+    /**
+     * A helper method to calculate RMSE.
+     */
+    Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations,
+                                  const std::vector<Eigen::VectorXd> &ground_truth);
 
-  /**
-   * A helper method to calculate Jacobians.
-   */
-  Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
+    /**
+     * A helper method to calculate Jacobians.
+     */
+    Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd &x_state);
 
+    /**
+     * Method converting Polar coordinates to Cartesian ones.
+     * @param measurement_pack
+     * @return
+     */
+    Eigen::VectorXd Polar2Cartesian(const MeasurementPackage &measurement_pack);
+
+    /**
+     * Verifies if value is smaller than Tools::epsilon
+     * @param value
+     * @return
+     */
+    double verifyZero(double value);
 };
 
 #endif  // TOOLS_H_
